e289e4dec5aaf478ff76e036816b67973fe3aa9a
Jaydee <108048499+JoseDexterAnyayahan@users.noreply.github.com>
2023-11-03 06:19:07 +0800

    1

---
+++ src/layouts/components/DrawerContent.vue
@@ -0,0 +1,106 @@
+<script setup>
+import upgradeBannerDark from '@/assets/images/pro/upgrade-banner-dark.png'
+import upgradeBannerLight from '@/assets/images/pro/upgrade-banner-light.png'
+import logo from '@/assets/logo.svg?raw'
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+import { useTheme } from 'vuetify'
+
+const vuetifyTheme = useTheme()
+const upgradeBanner = computed(() => {
+  return vuetifyTheme.global.name.value === 'light' ? upgradeBannerLight : upgradeBannerDark
+})
+</script>
+
+<template>
+  <!-- 👉 Nav header -->
+  <div class="nav-header">
+    <RouterLink
+      to="/"
+      class="app-logo d-flex align-center gap-x-3 app-title-wrapper"
+    >
+      <!-- ℹ️ You can also use img tag or VImg here -->
+      <div v-html="logo" />
+
+      <Transition name="vertical-nav-app-title">
+        <h1 class="font-weight-semibold leading-normal text-xl text-uppercase">
+          PDEA
+        </h1>
+      </Transition>
+    </RouterLink>
+  </div>
+
+  <!-- 👉 Nav items -->
+  <ul>
+    <VerticalNavLink
+      :item="{
+        title: 'Dashboard',
+        to: 'index',
+        icon: { icon: 'mdi-home-outline' }
+      }"
+    />
+    <VerticalNavLink
+      :item="{
+        title: 'Account Settings',
+        to: 'account-settings',
+        icon: { icon: 'mdi-account-cog-outline' }
+      }"
+    />
+    <!-- 👉 Pages -->
+    <VerticalNavSectionTitle :item="{ heading: 'Pages' }" />
+    <VerticalNavLink
+      :item="{
+        title: 'Login',
+        to: 'login',
+        target: '_blank',
+        icon: { icon: 'mdi-login' }
+      }"
+    />
+
+    <!-- ℹ️ This path doesn't exist so 404 route will catch this undefined path -->
+    <VerticalNavLink
+      :item="{
+        title: 'Error',
+        to: { path: '/error' },
+        target: '_blank',
+        icon: { icon: 'mdi-alert-circle-outline' }
+      }"
+    />
+
+    <!-- 👉 Apps -->
+    <VerticalNavSectionTitle :item="{ heading: 'Apps' }" />
+  </ul>
+    <VerticalNavLink
+      :item="{
+        title: 'Logout',
+        to: 'login',
+        target: '_blank',
+        icon: { icon: 'mdi-login' }
+      }"
+    />
+</template>
+
+<style lang="scss">
+// .v-navigation-drawer {
+//   height: 100%;
+
+//   .v-navigation-drawer__content {
+//     display: flex;
+//     flex-direction: column;
+
+//     > ul {
+//       flex-grow: 1;
+//     }
+//   }
+// }
+
+.upgrade-banner {
+  margin-top: auto;
+  // position: absolute;
+  // bottom: 13px;
+  // left: 50%;
+  // transform: translateX(-50%);
+}
+</style>
